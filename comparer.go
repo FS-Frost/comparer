847f4bf176f89d6a0e51c7ceb23b0fe8c0ec5409
FS-Frost <30810344+FS-Frost@users.noreply.github.com>
2022-05-27 20:27:58 -0400

    Fix comparison.

--- comparer.go
+++ comparer.go
@@ -1,58 +1,46 @@
 package comparer
 
-import "fmt"
-
 func AreEqual(list1, list2 []string) (bool, int) {
-	big := list1
-	small := list2
+	bigList := list1
+	smallList := list2
 	if len(list2) > len(list1) {
-		big = list2
-		small = list1
+		bigList = list2
+		smallList = list1
 	}
 
-	fmt.Printf("big: %#v\n", big)
-	fmt.Printf("small: %#v\n", small)
+	bigMap := mapList(bigList)
+	smallMap := mapList(smallList)
 
-	var deltaBig, deltaSmall int
-	for i := 0; i < len(big); {
-		fmt.Printf("\ni: %d\n", i)
-
-		indexBig := i + deltaBig
-		indexSmall := i + deltaSmall
-		fmt.Printf("indexBig: %d\n", indexBig)
-		fmt.Printf("indexSmall: %d\n", indexSmall)
-
-		if len(small) == indexSmall || len(big) == indexBig {
-			return true, 0
+	for i := 0; i < len(bigMap); i++ {
+		lineBig := bigMap[i]
+		lineSmall, ok := smallMap[i]
+		if !ok || lineBig != lineSmall {
+			return false, lineBig.index
 		}
-
-		if len(small)-1 < indexSmall {
-			fmt.Printf("Big (%d) has more elements than small (%d)\n", len(big), len(small))
-			return false, i
-		}
-
-		stringBig := big[indexBig]
-		stringSmall := small[indexSmall]
-
-		if stringBig == "" {
-			fmt.Printf("Skip empty string in big[%d]\n", indexBig)
-			deltaBig++
-			continue
-		}
-
-		if stringSmall == "" {
-			fmt.Printf("Skip empty string in small[%d]\n", indexSmall)
-			deltaSmall++
-			continue
-		}
-
-		if stringBig != stringSmall {
-			return false, i
-		}
-
-		fmt.Println("Equals. Next")
-		i++
 	}
 
 	return true, 0
+}
+
+type line struct {
+	index   int
+	content string
+}
+
+func mapList(list []string) map[int]line {
+	lines := map[int]line{}
+	mapIndex := 0
+	for i := 0; i < len(list); i++ {
+		item := list[i]
+		if item == "" {
+			continue
+		}
+
+		lines[mapIndex] = line{
+			index:   i,
+			content: item,
+		}
+		mapIndex++
+	}
+	return lines
 }
--- comparer_test.go
+++ comparer_test.go
@@ -1,42 +1,16 @@
 package comparer_test
 
 import (
+	"bufio"
+	"fmt"
+	"os"
+	"path"
 	"testing"
 
 	"github.com/FS-Frost/comparer"
 )
 
 func TestAreEqual(t *testing.T) {
-	t.Skip()
-	a1 := []string{"a", "b", "c"}
-	a2 := []string{"a", "b", "c"}
-	areEqual, index := comparer.AreEqual(a1, a2)
-
-	if index != 0 {
-		t.Errorf("index should be 0, instead: %d\n", index)
-	}
-
-	if areEqual == false {
-		t.Errorf("should be equal, instead '%t' at index '%d'\n", areEqual, index)
-	}
-}
-
-func TestAreNotEqual(t *testing.T) {
-	t.Skip()
-	a1 := []string{"a", "b", "c"}
-	a2 := []string{"a", "f", "c"}
-	areEqual, index := comparer.AreEqual(a1, a2)
-
-	if index != 1 {
-		t.Errorf("index should be 1, instead: %d\n", index)
-	}
-
-	if areEqual == true {
-		t.Errorf("should not be equal, found difference at index '%d'\n", index)
-	}
-}
-
-func TestDistinctLenArraysAreNotEqual(t *testing.T) {
 	type testCase struct {
 		list1          []string
 		list2          []string
@@ -47,21 +21,21 @@ func TestDistinctLenArraysAreNotEqual(t *testing.T) {
 	cases := []testCase{
 		{
 			list1:          []string{"a"},
-			list2:          []string{"a", "b"},
-			expectedResult: false,
-			expectedIndex:  1,
-		},
-		{
-			list1:          []string{"a"},
 			list2:          []string{"a"},
 			expectedResult: true,
 			expectedIndex:  0,
 		},
 		{
-			list1:          []string{"a", "b", "c"},
+			list1:          []string{"a"},
+			list2:          []string{"b"},
+			expectedResult: false,
+			expectedIndex:  0,
+		},
+		{
+			list1:          []string{"a"},
 			list2:          []string{"a", "b"},
 			expectedResult: false,
-			expectedIndex:  2,
+			expectedIndex:  1,
 		},
 		{
 			list1:          []string{"a", "", "b"},
@@ -69,21 +43,96 @@ func TestDistinctLenArraysAreNotEqual(t *testing.T) {
 			expectedResult: true,
 			expectedIndex:  0,
 		},
+		{
+			list1:          []string{"a", "", "b"},
+			list2:          []string{"a", "", "c"},
+			expectedResult: false,
+			expectedIndex:  2,
+		},
+		{
+			list1:          []string{"a", ""},
+			list2:          []string{"a"},
+			expectedResult: true,
+			expectedIndex:  0,
+		},
+		{
+			list1:          []string{"a", "", ""},
+			list2:          []string{"a", ""},
+			expectedResult: true,
+			expectedIndex:  0,
+		},
+		{
+			list1:          []string{"a", "b", "c"},
+			list2:          []string{"a", "f", "c"},
+			expectedResult: false,
+			expectedIndex:  1,
+		},
+		{
+			list1:          []string{"a", "b", "c"},
+			list2:          []string{"a", "b", "c"},
+			expectedResult: true,
+			expectedIndex:  0,
+		},
 	}
 
 	for i, c := range cases {
-		compareDistinctLenArrays(t, i+1, c.list1, c.list2, c.expectedIndex, c.expectedResult)
+		caseNumber := i + 1
+		caseName := fmt.Sprintf("%d", caseNumber)
+		compareLists(t, caseName, c.list1, c.list2, c.expectedIndex, c.expectedResult)
+
+		caseName = fmt.Sprintf("%d inverted", caseNumber)
+		compareLists(t, caseName, c.list2, c.list1, c.expectedIndex, c.expectedResult)
 	}
 }
 
-func compareDistinctLenArrays(t *testing.T, caseIndex int, list1, list2 []string, expectedIndex int, expectedResult bool) {
-	areEqual, index := comparer.AreEqual(list1, list2)
-
-	if index != expectedIndex {
-		t.Errorf("case %d: index should be %d, instead: %d\n", caseIndex, index, expectedIndex)
+func TestFilesAreEqual(t *testing.T) {
+	cwd, err := os.Getwd()
+	if err != nil {
+		t.Fatalf("error getting working directory: %v", err)
 	}
 
-	if areEqual != expectedResult {
-		t.Errorf("case %d: equality should be %t, instead found difference at index '%d'\n", caseIndex, expectedResult, index)
+	lines1, err := readLines(path.Join(cwd, "data/file1.ass"))
+	if err != nil {
+		t.Fatalf("error reading test file: %v", err)
 	}
+
+	lines2, err := readLines(path.Join(cwd, "data/file2.ass"))
+	if err != nil {
+		t.Fatalf("error reading test file: %v", err)
+	}
+
+	compareLists(t, "ASS files", lines1, lines2, 19, false)
+}
+
+func compareLists(t *testing.T, caseName string, list1, list2 []string, expectedIndex int, expectedResult bool) {
+	actualResult, actualIndex := comparer.AreEqual(list1, list2)
+	printLists := func() {
+		t.Logf("%#v\n", list1)
+		t.Logf("%#v\n", list2)
+	}
+
+	if actualResult != expectedResult {
+		printLists()
+		t.Fatalf("case %s: equality should be %t, instead found %t with index '%d'\n", caseName, expectedResult, actualResult, actualIndex)
+	}
+
+	if actualIndex != expectedIndex {
+		printLists()
+		t.Fatalf("case %s: index should be %d, instead found %d\n", caseName, expectedIndex, actualIndex)
+	}
+}
+
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
 }
